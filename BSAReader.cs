af20ac72f9c287f8ed1693164248248e85a17059
scorpion <scorpion.jze@gmail.com>
2014-06-09 03:18:46 -0500

    hash algorithm and debug project

--- BSAReader.cs
+++ BSAReader.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BSAsharp.Format;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -7,28 +9,133 @@ using System.Threading.Tasks;
 
 namespace BSAsharp
 {
-    public class BSAReader
+    public class BSAReader : IDisposable
     {
-        private readonly Stream _bsa;
+        private readonly BinaryReader _reader;
+
         public BSAReader(Stream bsaStream)
         {
-            this._bsa = bsaStream;
+            this._reader = new BinaryReader(bsaStream);
+        }
+        ~BSAReader()
+        {
+            Dispose(false);
         }
 
-        public void Shit()
+        protected virtual void Dispose(bool disposing)
         {
-            using (var br = new BinaryReader(_bsa))
+            if (disposing)
             {
-                var field = br.ReadChars(4);
-                var version = br.ReadUInt32();
-                var offset = br.ReadUInt32();
-                var archiveFlags = br.ReadUInt32();
-                var folderCount = br.ReadUInt32();
-                var fileCount = br.ReadUInt32();
-                var totalFolderNameLength = br.ReadUInt32();
-                var totalFileNameLength = br.ReadUInt32();
-                var fileFlags = br.ReadUInt32();
+                _reader.Dispose();
             }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Read()
+        {
+            ReadHeader();
+        }
+
+        private void ReadHeader()
+        {
+            var field = _reader.ReadChars(4);
+            var version = _reader.ReadUInt32();
+            var offset = _reader.ReadUInt32();
+            var archiveFlags = (ArchiveFlags)_reader.ReadUInt32();
+            var folderCount = _reader.ReadUInt32();
+            var fileCount = _reader.ReadUInt32();
+            var totalFolderNameLength = _reader.ReadUInt32();
+            var totalFileNameLength = _reader.ReadUInt32();
+            var fileFlags = (FileFlags)_reader.ReadUInt32();
+
+            var folderDict = new Dictionary<string, List<FileRecord>>();
+
+            var folders = ReadFolderRecord(folderCount);
+            foreach (var folder in folders)
+            {
+                string name;
+                var frbs = ReadFileRecordBlocks(folder.count, out name);
+
+                folderDict.Add(name, frbs);
+            }
+
+            //archiveFlags.HasFlag(ArchiveFlags.Compressed)
+            var fileNames = ReadFileNameBlocks(fileCount);
+            foreach (var fn in fileNames)
+            {
+                //var hash = CreateHash(Path.Combine(folderDict.Keys.First(), Path.GetFileNameWithoutExtension(fn)), Path.GetExtension(fn));
+                var hash = CreateHash(Path.GetFileNameWithoutExtension(fn), Path.GetExtension(fn));
+                var match = folderDict.SelectMany(kvp => kvp.Value.Where(fr => fr.hash == hash)).FirstOrDefault();
+                Console.WriteLine(hash);
+            }
+        }
+
+        private List<FolderRecord> ReadFolderRecord(uint count)
+        {
+            var folders = new List<FolderRecord>((int)count);
+            for (uint i = 0; i < count; i++)
+                folders.Add(_reader.ReadStruct<FolderRecord>());
+
+            return folders;
+        }
+
+        private List<FileRecord> ReadFileRecordBlocks(uint count, out string name)
+        {
+            var fileRecords = new List<FileRecord>();
+
+            name = _reader.ReadBString(true);
+            for (uint i = 0; i < count; i++)
+                fileRecords.Add(_reader.ReadStruct<FileRecord>());
+
+            return fileRecords;
+        }
+
+        private List<string> ReadFileNameBlocks(uint count)
+        {
+            var fileNames = new List<string>();
+            for (int i = 0; i < count; i++)
+                fileNames.Add(_reader.ReadCString());
+
+            return fileNames;
+        }
+
+        private ulong CreateHash(string file, string ext)
+        {
+            Trace.Assert(file.Length > 0);
+            ulong hash1 = (ulong)(file[file.Length - 1] | ((file.Length > 2 ? file[file.Length - 2] : 0) << 8) | file.Length << 16 | file[0] << 24);
+
+            Trace.Assert(ext.Length > 0);
+            switch (ext)
+            {
+                case ".kf":
+                    hash1 |= 0x80;
+                    break;
+                case ".nif":
+                    hash1 |= 0x8000;
+                    break;
+                case ".dds":
+                    hash1 |= 0x8080;
+                    break;
+                case ".wav":
+                    hash1 |= 0x80000000;
+                    break;
+            }
+
+            ulong hash2 = 0, hash3 = 0;
+            if (file.Length > 3)
+                for (int i = 1; i < file.Length - 2; i++)
+                    hash2 = (hash2 * 0x1003F) + file[i];
+
+            for (int i = 0; i < ext.Length; i++)
+                hash3 = (hash3 * 0x1003F) + ext[i];
+
+            hash2 = ((hash2 << 32) + (hash3 << 32));
+
+            return hash2 + hash1;
         }
     }
 }
---
+++ BSAsharp-debug/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSAsharp_debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fs = File.OpenRead(@"X:\Games\Fallout3\Data\Fallout - Misc.bsa"))
+            {
+                var x = new BSAsharp.BSAReader(fs);
+                x.Read();
+            }
+        }
+    }
+}

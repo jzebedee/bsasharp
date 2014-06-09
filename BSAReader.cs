c0c87713b8bb3c620ee7a46b0ce51b611ec33f30
scorpion <scorpion.jze@gmail.com>
2014-06-09 01:22:31 -0500

    added BSA# project

---
+++ BSAReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSAsharp
+{
+    public class BSAReader
+    {
+        private readonly Stream _bsa;
+        public BSAReader(Stream bsaStream)
+        {
+            this._bsa = bsaStream;
+        }
+
+        public void Shit()
+        {
+            using (var br = new BinaryReader(_bsa))
+            {
+                var field = br.ReadChars(4);
+                var version = br.ReadUInt32();
+                var offset = br.ReadUInt32();
+                var archiveFlags = br.ReadUInt32();
+                var folderCount = br.ReadUInt32();
+                var fileCount = br.ReadUInt32();
+                var totalFolderNameLength = br.ReadUInt32();
+                var totalFileNameLength = br.ReadUInt32();
+                var fileFlags = br.ReadUInt32();
+            }
+        }
+    }
+}

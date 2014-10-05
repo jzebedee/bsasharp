4f83441700c83f72d2b5a5b876502adbaa4094f2
scorpion <scorpion.jze@gmail.com>
2014-10-05 16:25:08 -0500

    Added reactive

--- bsasharp-ui/frmBsaView.Designer.cs
+++ bsasharp-ui/frmBsaView.Designer.cs
@@ -28,33 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
             this.treeBsa = new System.Windows.Forms.TreeView();
+            this.ttipBsa = new System.Windows.Forms.ToolTip(this.components);
+            this.dlgOpenBsa = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.treeBsa);
+            this.splitContainer.Size = new System.Drawing.Size(784, 561);
+            this.splitContainer.SplitterDistance = 353;
+            this.splitContainer.TabIndex = 1;
             // 
             // treeBsa
             // 
             this.treeBsa.Dock = System.Windows.Forms.DockStyle.Fill;
             this.treeBsa.Location = new System.Drawing.Point(0, 0);
             this.treeBsa.Name = "treeBsa";
-            this.treeBsa.Size = new System.Drawing.Size(284, 261);
-            this.treeBsa.TabIndex = 0;
-            this.treeBsa.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeBsa_AfterExpand);
+            this.treeBsa.Size = new System.Drawing.Size(353, 561);
+            this.treeBsa.TabIndex = 1;
+            // 
+            // dlgOpenBsa
+            // 
+            this.dlgOpenBsa.DefaultExt = "bsa";
+            this.dlgOpenBsa.Filter = "Bethesda Archive|*.bsa";
             // 
             // frmBsaView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.treeBsa);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.splitContainer);
             this.Name = "frmBsaView";
             this.Text = "BSA Viewer";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.SplitContainer splitContainer;
         private System.Windows.Forms.TreeView treeBsa;
+        private System.Windows.Forms.ToolTip ttipBsa;
+        private System.Windows.Forms.OpenFileDialog dlgOpenBsa;
+
     }
 }
 
--- bsasharp-ui/frmBsaView.cs
+++ bsasharp-ui/frmBsaView.cs
@@ -21,25 +21,8 @@ namespace bsasharp_ui
             var bsaTree = new BsaTree(testBsa);
 
             treeBsa.Nodes.AddRange(bsaTree.Nodes.ToArray());
-        }
 
-        private void treeBsa_AfterExpand(object sender, TreeViewEventArgs e)
-        {
-            var expando = e.Node.Tag as IDictionary<string, object>;
-            var nodes = e.Node.Nodes;
-            nodes.RemoveByKey("_");
-
-            if (expando != null)
-            {
-                foreach (var kvp in expando)
-                {
-                    var newNode = new TreeNode(kvp.Key) { Tag = kvp.Value };
-                    if (kvp.Value is IDictionary<string, object>)
-                        newNode.Nodes.Add("_", "");
-                    nodes.Add(newNode);
-                }
-                e.Node.Tag = null;
-            }
+            dlgOpenBsa.ShowDialog();
         }
     }
 }

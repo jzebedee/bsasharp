3e744825a16004cad7f77350d12ae104cfce9ffe
J. Zebedee <zebedee@code.gripe>
2017-07-03 23:48:11 -0500

    Remove unks in ArchiveFlags, rename Compressed to DefaultCompressed

--- bsasharp/Format/ArchiveFlags.cs
+++ bsasharp/Format/ArchiveFlags.cs
@@ -7,14 +7,7 @@ namespace BSAsharp.Format
     {
         NamedDirectories = 1 << 0,
         NamedFiles = 1 << 1,
-        Compressed = 1 << 2,
-        unk1 = 1 << 3,
-        unk2 = 1 << 4,
-        unk3 = 1 << 5,
-        unk4 = 1 << 6,
-        unk5 = 1 << 7,
+        DefaultCompressed = 1 << 2,
         BStringPrefixed = 1 << 8,
-        unk6 = 1 << 9,
-        unk7 = 1 << 10
     }
 }

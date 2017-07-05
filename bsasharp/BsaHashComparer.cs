527a780e1d7cbebd92e93fb4666deb17b16948e0
J. Zebedee <zebedee@code.gripe>
2017-07-04 23:37:00 -0500

    Use Lazy for BsaHashComparer singleton

--- bsasharp/BsaHashComparer.cs
+++ bsasharp/BsaHashComparer.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace BSAsharp
 {
-    sealed class BsaHashComparer : IComparer<IBsaEntry>
+    internal sealed class BsaHashComparer : IComparer<IBsaEntry>
     {
         //Comparisons are based ONLY on the hash, which is created only
         //from the filename in BSAFiles and the path in BSAFolders.
         //Don't try to use comparisons OUTSIDE of the folder!!
-        public static readonly BsaHashComparer Instance = new BsaHashComparer();
+        private static readonly Lazy<BsaHashComparer> _instance = new Lazy<BsaHashComparer>(() => new BsaHashComparer());
+        internal static BsaHashComparer Instance => _instance.Value;
+
         private BsaHashComparer()
         {
-            
         }
 
         public int Compare(IBsaEntry x, IBsaEntry y)

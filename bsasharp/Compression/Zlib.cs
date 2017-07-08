b8945b6c1c22be395f5d1cd7e82b5c2daa422cbc
J. Zebedee <zebedee@code.gripe>
2017-07-08 14:53:17 -0500

    Add ZlibStream

---
+++ bsasharp/Compression/Zlib.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace BSAsharp.Compression
+{
+    public class Zlib
+    {
+        public byte[] Decompress(Stream compressedStream)
+        {
+            using (var msDecompressed = new MemoryStream())
+            using (var infStream = new ZlibStream(compressedStream, CompressionMode.Decompress))
+            {
+                infStream.CopyTo(msDecompressed);
+                return msDecompressed.ToArray();
+            }
+        }
+
+        //public byte[] Compress(Stream inputStream, CompressionLevel level = CompressionLevel.Optimal, bool leaveOpen = true)
+        //{
+        //    using (var msCompressed = new MemoryStream())
+        //    using (var defStream = MakeZlibDeflateStream(msCompressed, level, leaveOpen))
+        //    {
+        //        inputStream.CopyTo(defStream);
+        //        return msCompressed.ToArray();
+        //    }
+        //}
+
+        public virtual Stream CompressStream(Stream msCompressed, CompressionLevel level, bool leaveOpen = true)
+        {
+            return new ZlibStream(msCompressed, level, leaveOpen);
+        }
+    }
+}
---
+++ bsasharp/Compression/ZlibStream.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace BSAsharp.Compression
+{
+    public class ZlibStream : DeflateStream
+    {
+        private static readonly byte[] HeaderMagic = { 0x78, 0x01 }; //CMF, FLG
+
+        public ZlibStream(Stream stream, CompressionLevel compressionLevel) : base(stream, compressionLevel)
+        {
+            WriteZlibHeader(stream);
+        }
+
+        public ZlibStream(Stream stream, CompressionLevel compressionLevel, bool leaveOpen) : base(stream, compressionLevel, leaveOpen)
+        {
+            WriteZlibHeader(stream);
+        }
+
+        public ZlibStream(Stream stream, CompressionMode mode) : base(stream, mode)
+        {
+            if (mode == CompressionMode.Decompress) SkipZlibHeader(stream);
+            else WriteZlibHeader(stream);
+        }
+
+        public ZlibStream(Stream stream, CompressionMode mode, bool leaveOpen) : base(stream, mode, leaveOpen)
+        {
+            if (mode == CompressionMode.Decompress) SkipZlibHeader(stream);
+            else WriteZlibHeader(stream);
+        }
+
+        private void SkipZlibHeader(Stream s) => s.Seek(HeaderMagic.Length, SeekOrigin.Current);
+        private void WriteZlibHeader(Stream s) => s.Write(HeaderMagic, 0, HeaderMagic.Length);
+    }
+}

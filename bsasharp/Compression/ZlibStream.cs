307ee9506975ef6a01a71189efd47c42f5f94fa8
J. Zebedee <zebedee@code.gripe>
2017-07-08 17:37:08 -0500

    Implement more of Zlib format in ZlibStream

--- bsasharp/Compression/ZlibStream.cs
+++ bsasharp/Compression/ZlibStream.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 
@@ -10,24 +11,41 @@ namespace BSAsharp.Compression
 
         private readonly byte[] Header = new byte[ZlibHeaderSize];
 
-        public int CompressionMethod => Header[0] & 0b1111;
+        public int CompressionMethod
+        {
+            get => Header[0] & 0b1111;
+            set => Header[0] |= (byte)(value & 0b1111);
+        }
+        public int CompressionInfo
+        {
+            get => (Header[0] & 0b11110000) >> 4;
+            set => Header[0] |= (byte)((value << 4) & 0b11110000);
+        }
 
-        public int CompressionInfo => (Header[0] & 0b11110000) >> 4;
-
-        public int FlagCheck => Header[1] & 0b1111;
-
-        public bool FlagPresetDictionary => (Header[1] & 0b10000) != 0 ? true : false;
-
-        public int FlagCompressionLevel => (Header[1] & 0b11000000) >> 6;
+        public int FlagCheck
+        {
+            get => Header[1] & 0b1111;
+            set => Header[1] |= (byte)(value & 0b1111);
+        }
+        public bool FlagPresetDictionary
+        {
+            get => (Header[1] & 0b10000) != 0 ? true : false;
+            set => Header[1] |= (byte)(value ? 0b10000 : 0);
+        }
+        public int FlagCompressionLevel
+        {
+            get => (Header[1] & 0b11000000) >> 6;
+            set => Header[1] |= (byte)((value << 6) & 0b11000000);
+        }
 
         public ZlibStream(Stream stream, CompressionLevel compressionLevel) : base(stream, compressionLevel)
         {
-            WriteZlibHeader(stream);
+            WriteZlibHeader(stream, compressionLevel);
         }
 
         public ZlibStream(Stream stream, CompressionLevel compressionLevel, bool leaveOpen) : base(stream, compressionLevel, leaveOpen)
         {
-            WriteZlibHeader(stream);
+            WriteZlibHeader(stream, compressionLevel);
         }
 
         public ZlibStream(Stream stream, CompressionMode mode) : base(stream, mode)
@@ -42,15 +60,45 @@ namespace BSAsharp.Compression
             else WriteZlibHeader(stream);
         }
 
+        private bool ValidateHeader(byte cmf, byte flg)
+        {
+            var shortHeader = (ushort)(cmf * 256 + flg);
+            return shortHeader % 31 == 0;
+        }
         private void ReadZlibHeader(Stream s)
         {
             if (s.Read(Header, 0, Header.Length) != Header.Length)
                 throw new InvalidOperationException("Header was not found");
 
-            var shortHeader = (ushort)(Header[0] * 256 + Header[1]);
-            if (shortHeader % 31 != 0)
+            if (!ValidateHeader(Header[0], Header[1]))
                 throw new InvalidOperationException("Header is corrupt");
         }
-        private void WriteZlibHeader(Stream s) => s.Write(Header, 0, Header.Length);
+        private void WriteZlibHeader(Stream s, CompressionLevel? compressionLevel = null)
+        {
+            if (compressionLevel != null)
+            {
+                switch (compressionLevel.Value)
+                {
+                    case CompressionLevel.Fastest:
+                        FlagCompressionLevel = 0;
+                        CompressionMethod = 8;
+                        CompressionInfo = 7;
+                        throw new NotImplementedException(nameof(CompressionLevel.Fastest));
+                        break;
+                    case CompressionLevel.Optimal:
+                        CompressionMethod = 8;
+                        CompressionInfo = 7;
+                        FlagCheck = 0b1010;
+                        FlagPresetDictionary = true;
+                        FlagCompressionLevel = 3;
+                        break;
+                    case CompressionLevel.NoCompression:
+                        throw new NotImplementedException(nameof(CompressionLevel.NoCompression));
+                }
+            }
+
+            Debug.Assert(ValidateHeader(Header[0], Header[1]));
+            s.Write(Header, 0, Header.Length);
+        }
     }
 }
